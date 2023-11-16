22b060047d90b31f080071e143f74a3f95d3e996
jose.pena <ze__augusto@hotmail.com>
2023-11-16 20:09:50 -0300

    Chore: Add new route and use appointment service

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-// Instanciamento
+// Serviços
+const AppointmentService = require('./services/Appointment')
+
+// Instânciamento
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -13,14 +16,33 @@ app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 
 // Banco
-mongoose.connect(
-  'mongodb://localhost:27017/agendamento',
-  { useNewUrlParser: true, useUnifiedTopology: true }
-)
+mongoose.connect('mongodb://127.0.0.1:27017/agendamento')
 
 // Roteamento
 app.get('/', (req, res) => {
-  res.send('Oi!')
+  res.send('API rodando com sucesso!')
+})
+
+app.get('/cadastro', (req, res) => {
+  res.render('create')
+})
+
+app.post('/create', async (req, res) => {
+  const status = await AppointmentService.Create(
+    req.body.name,
+    req.body.email,
+    req.body.description,
+    req.body.cpf,
+    req.body.date,
+    req.body.time,
+  )
+
+  if (status) {
+    console.log('show de bola!')
+    res.redirect('/')
+  } else {
+    res.send('Deu ruim!')
+  }
 })
 
 // Inicialização
