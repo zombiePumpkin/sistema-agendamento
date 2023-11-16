7cb8f19248f838e830842fdd24e39dc1973b1c79
jose.pena <ze__augusto@hotmail.com>
2023-11-16 20:08:36 -0300

    Feat: Create appointment model

---
+++ models/Appointment.js
@@ -0,0 +1,15 @@
+// Dependencias
+const mongoose = require('mongoose')
+
+// Schema das informações da tabela
+const Appointment = new mongoose.Schema({
+  name: String,
+  email: String,
+  description: String,
+  cpf: String,
+  time: String,
+  isFinished: Boolean
+})
+
+// Exportando o Schema como um módulo
+module.exports = Appointment
