c24dfe8f05573ec30db427d0ca7b601a84582271
jose.pena <ze__augusto@hotmail.com>
2023-11-17 18:02:46 -0300

    Feat: Create Appointment factory

---
+++ factories/Appointment.js
@@ -0,0 +1,19 @@
+class Appointment {
+  Build(appointment) {
+    const day = appointment.date.getDate() + 1
+    const month = appointment.date.getMonth()
+    const year = appointment.date.getFullYear()
+    const [hour, minutes] = appointment.time.split(':')
+    const startDate = new Date(year, month, day, hour, minutes, 0, 0)
+    // startDate.setHours(startDate.getHours() - 3) // Converte para o horário padrão Brasileiro (-3hrs)
+
+    return {
+      id: appointment._id,
+      title: `${appointment.name} - ${appointment.description}`,
+      start: startDate,
+      end: startDate,
+    }
+  }
+}
+
+module.exports = new Appointment() 
