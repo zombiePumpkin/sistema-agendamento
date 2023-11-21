c8424a819d3251e7511d5a57c242d56f5fb0952e
jose.pena <ze__augusto@hotmail.com>
2023-11-20 23:31:56 -0300

    Chore: Add polling to send email notifications

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs')
 
 // Banco
 mongoose.connect('mongodb://127.0.0.1:27017/agendamento')
-
+ 
 // Roteamento
 app.get('/', (req, res) => {
   res.render('index')
@@ -61,6 +61,38 @@ app.get('/search', async (req, res) => {
     res.send('Deu ruim!')
   }
 })
+
+app.post('/finish', async (req, res) => {
+  const id = req.body.id
+  await AppointmentService.FinishAppointment(id)
+  res.redirect('/')
+})
+
+app.get('/list', async (req, res) => {
+  const appointmentList = await AppointmentService.GetAll(true)
+  if (appointmentList) {
+    res.render('list', { appointmentList })
+  } else {
+    res.render('Deu ruim!')
+  }
+})
+
+app.get('/searchResult', async (req, res) => {
+  const query = req.query.search
+  const appointmentList = await AppointmentService.Find(query)
+  if (appointmentList) {
+    return res.render('list', { appointmentList })
+  } else {
+    res.render('Deu ruim!')
+  }
+})
+
+// Polling
+const pollTime = 1000 * 60 * 60 // 1 hora
+setInterval(async () => {
+  AppointmentService.SendNotification()
+}, pollTime)
+
 
 // Inicialização
 app.listen(8080, () => {})
